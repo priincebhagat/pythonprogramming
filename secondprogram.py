eb3c3ac1ffebb4cfbc55f79c8842f6c55c2362e7
priincebhagat <bhagatprince630@gmail.com>
2026-06-16 16:24:56 +0530

    Add secondprogram.py

---
+++ secondprogram.py
@@ -0,0 +1,13 @@
+#Indexing
+str = "Prince Bhagat"
+print(str[3])
+
+#Slicing
+print(str[0:7])
+#we have one option to write an ending index instead of integer
+print(str[0:len(str)])
+
+#Q. Write a program to print user's first name and it's length
+name = input("Enter your First name : ")
+print("length of your name : ",len(name))
+
