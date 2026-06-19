445a7e9b052737e986eb58be7bc215bfb30b40fe
priincebhagat <bhagatprince630@gmail.com>
2026-06-19 09:27:25 +0530

    sets

--- Dictionaries&Sets.py
+++ Dictionaries&Sets.py
@@ -26,3 +26,25 @@ print(student.get("age"))
 new_dict = {"city" : "Palghar"}
 student.update(new_dict)
 print(student)
+
+#Set
+# A set is an unordered collection of unique elements, 
+# defined using curly braces `{ }` or the `set()` constructor. 
+# Sets are mutable, allowing for the addition and removal of elements, 
+# but they do not support indexing or slicing. 
+
+collection = {1,2,3,3,4,5}
+print("collection : ",collection)
+
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+my_set.add(3)
+print("my_set : ",my_set)
+
+my_set.remove(3)
+print("my_set after removing 3 : ",my_set)
+
+popping = { "Prince" , "Bishal" , "Rohit" , "Hriday"}
+print(popping.pop())
+
