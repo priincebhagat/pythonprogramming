fd0f263cf14f2acee8b2756e17c56b91bfc9f4e6
priincebhagat <bhagatprince630@gmail.com>
2026-06-24 17:26:17 +0530

    Delete practice.py

--- practice.py
+++
@@ -1,8 +0,0 @@
-#Ask the user for a total bill amount and the number of people. Calculate and print each person's share, and also show the amount with a 10% tip included.
-
-total_bill = float(input("Enter the total bill amount : "))
-number_of_people = int(input("Enter the number of people : "))
-share_per_person = total_bill / number_of_people
-with_tip = total_bill * 1.10 / number_of_people
-print("Each person's share is : ",share_per_person)
-print("Each person's share with tip is : ",with_tip)
