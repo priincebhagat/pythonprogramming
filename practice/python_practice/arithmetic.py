7494524b41608e8308a51e6147e20fccbfa27992
priincebhagat <bhagatprince630@gmail.com>
2026-06-24 17:32:02 +0530

    Add practice problem files

---
+++ practice/python_practice/arithmetic.py
@@ -0,0 +1,12 @@
+# Problem 1 - Bill Splitter
+
+total_bill = float(input("Enter the total bill amount : "))
+number_of_people = int(input("Enter the number of people : "))
+share_per_person = total_bill / number_of_people
+with_tip = (total_bill * 1.10) / number_of_people
+print("Each person's share is : ", share_per_person)
+print("Each person's share with tip is : ", with_tip)
+
+# Problem 2 - Temperature Converter
+
+# your code here
