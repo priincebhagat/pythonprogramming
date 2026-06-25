b1f75432c48e15954b27898d7f94d717b2941518
priincebhagat <bhagatprince630@gmail.com>
2026-06-25 18:06:35 +0530

    even number filter

--- practice/python_practice/lists.py
+++ practice/python_practice/lists.py
@@ -1,12 +1,16 @@
-# ============================================
 # Problem 1 - Student Marks Tracker
-# ============================================
+student_marks=[81, 87, 65, 86, 75, 68]
+print("Highest : ",max(student_marks))
+print("Lowest : ",min(student_marks))
+print("Average : ",sum(student_marks) / len(student_marks))
 
-# your code here
-
-
-# ============================================
 # Problem 2 - Even Number Filter
-# ============================================
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = []
+for number in numbers :
+    if number % 2 == 0 :
 
-# your code here
+        even_numbers.append(number)
+        
+print("Even Numbers : ", even_numbers)
+        
