efc25603b8c7542650ced5617da05528cc8323ac
priincebhagat <bhagatprince630@gmail.com>
2026-06-28 17:24:45 +0530

    contact info

--- practice/python_practice/dictionaries.py
+++ practice/python_practice/dictionaries.py
@@ -1,12 +1,12 @@
-# ============================================
 # Problem 1 - Contact Book
-# ============================================
+#Create a dictionary of 3 contacts with names as keys and phone numbers as values. Print each contact
+contacts = {
+    "Prince": 9730239470,
+    "Riya": 8765432987
+}
 
-# your code here
+for name, phone in contacts.items():
+    print("Name : ",name, ",  Phone : ",phone)
 
 
-# ============================================
 # Problem 2 - Word Frequency Counter
-# ============================================
-
-# your code here
