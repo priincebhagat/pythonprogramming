30a544de9181dbf4ebf36e6cfe66ae15f897fd31
priincebhagat <bhagatprince630@gmail.com>
2026-07-26 21:58:58 +0530

    reading lines through file i/o

--- practice/python_practice/file_input&output.py
+++ practice/python_practice/file_input&output.py
@@ -1,5 +1,13 @@
-f = open("practice 1","r")
-data = f.read()
-print(data)
+f = open("Loops","r")
+
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+line3 = f.readline()
+print(line3)
+
 f.close()
 
